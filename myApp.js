82624c7db7fcc43407f73975fe9fd500bb773909
Tsara Sudrajat <tsara.tf@gmail.com>
2022-01-27 05:23:22 +0700

    Add mongodb and mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
