c656d6018f1948804b35f55a87613c402c016241
Tsara Sudrajat <tsara.tf@gmail.com>
2022-01-27 12:49:54 +0700

    Add person model

--- myApp.js
+++ myApp.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 
 const mongoose = require("mongoose");
+const { Schema } =  mongoose;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+let Person = mongoose.modal('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
