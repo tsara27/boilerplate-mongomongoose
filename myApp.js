d3309c913e0b6c972f7ffa24c683580106abaf63
Tsara Sudrajat <tsara.tf@gmail.com>
2022-01-28 14:54:53 +0700

    Add find one and update function to change age

--- myApp.js
+++ myApp.js
@@ -84,7 +84,12 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, function(err, person) {
+    if (err) {
+      return console.error("ERROR");
+    }
+    done(null /*, data*/);
+  });
 };
 
 const removeById = (personId, done) => {
